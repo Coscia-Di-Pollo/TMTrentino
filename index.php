7041cf5b495ecaab441157fc0a7f867255124fbe
Coscia-Di-Pollo <riccardo.antonio.grisenti@gmail.com>
2026-09-23 17:30:39 +0200

    Inizio foglio di stile

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="style/style.css">
+
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    media="print"
+    onload="this.media = 'all'"
+  >
+
+  <title> TM Trentino - News del calcio Trentino </title>
+</head>
+<body>
+  <header>
+    <div class="header">
+      <!-- <div class="site__header"><figure><img src="media/logo.png" alt="Logo di TM Trentino, News del calcio trentino"></figure></div> -->
+      <nav class="nav__header">
+        <ul class="menu__header">
+          <li><a href="">Homepage</a></li>
+          <li><a href="">Chi siamo</a></li>
+          <li><a href="">Squadra</a></li>
+          <li><a href="">Contatti</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</body>
+</html>
